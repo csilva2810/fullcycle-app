0b25304a7ea94f5a59d7924b0bee2fe49b310e9a
Carlos Silva <csilva2810@gmail.com>
2021-04-08 09:57:46 -0300

    Add nest api producing to new-direction and consuming from new-position

---
+++ api/src/main.ts
@@ -0,0 +1,37 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+
+const getGroupId = () => {
+  const id = process.env.KAFKA_CONSUMER_GROUP_ID;
+
+  if (!id || id === '') {
+    return `my-consumer-${Math.random()}`;
+  }
+
+  return id;
+};
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      // @TODO se how to wait for the config modules to be ready to be able to inject the env vars below
+      client: {
+        // clientId: process.env.KAFKA_CLIENT_ID,
+        // brokers: [process.env.KAFKA_BROKER],
+        clientId: 'code-delivery',
+        brokers: ['host.docker.internal:9094'],
+      },
+      consumer: {
+        groupId: getGroupId(),
+      },
+    },
+  });
+
+  await app.startAllMicroservicesAsync();
+  await app.listen(3000);
+}
+bootstrap();
---
+++ api/src/routes/routes.module.ts
@@ -0,0 +1,49 @@
+import { Module } from '@nestjs/common';
+import { RoutesService } from './routes.service';
+import { RoutesController } from './routes.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Route, RouteSchema } from './entities/route.entity';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule } from '@nestjs/config';
+
+const getGroupId = () => {
+  const id = process.env.KAFKA_CONSUMER_GROUP_ID;
+
+  if (!id || id === '') {
+    return `my-consumer-${Math.random()}`;
+  }
+
+  return id;
+};
+
+@Module({
+  imports: [
+    ConfigModule,
+    MongooseModule.forFeature([{ name: Route.name, schema: RouteSchema }]),
+    ClientsModule.registerAsync([
+      {
+        name: 'KAFKA_SERVICE',
+        useFactory: async () => {
+          return {
+            transport: Transport.KAFKA,
+            options: {
+              // @TODO se how to wait for the config modules to be ready to be able to inject the env vars below
+              client: {
+                // clientId: process.env.KAFKA_CLIENT_ID,
+                // brokers: [process.env.KAFKA_BROKER],
+                clientId: 'code-delivery',
+                brokers: ['host.docker.internal:9094'],
+              },
+              consumer: {
+                groupId: getGroupId(),
+              },
+            },
+          };
+        },
+      },
+    ]),
+  ],
+  controllers: [RoutesController],
+  providers: [RoutesService],
+})
+export class RoutesModule {}
